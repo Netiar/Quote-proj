9df10f4f0e77871851e446d8d878cd149ed8db99
Raindrop <collins.odinga@student.moringaschool.com>
2022-04-12 00:30:51 +0300

    modified app quote components.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import {Quote} from "../quote"
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote( 1, 'You are never too old to set another goal or to dream a new dream',' — Malala Yousafzai',0,0,new Date(2020,3,14)),
-    new Quote(2, 'The greatest glory in living lies not in never falling, but in rising every time we fall.',' — Nelson Mandela', 0,0,new Date(2021,8,12)),
-    new Quote(3,'Never let the fear of striking out keep you from playing the game.','— Babe Ruth',0,0,new Date(2018,9,12)),
-    new Quote(4,'We all have problems. But it’s not what happens to us, [it’s] the choices we make after.','— Elizabeth Smart',0,0,new Date(2015,8,12))
+    new Quote( 1, 'Technology is best when it brings people together.',' — Matt Mullenweg,',0,0,new Date(2022,4,10)),
+    new Quote(2, 'It has become appallingly obvious that our technology has exceeded our humanity.',' — Albert Einstein, Scientist', 0,0,new Date(2022,4,10)),
+    new Quote(3,'Heroes share all their meat, but a pirate eats all the meat.','— Monkey D. Luffy',0,0,new Date(2022,4,10)),
+    new Quote(4,'It is only when they go wrong that machines remind you how powerful they are.','—  Clive James,',0,0,new Date(2022,4,10))
   
   ]
   
@@ -60,4 +60,4 @@ export class QuoteComponent implements OnInit {
     ngOnInit(): void {
     }
   
-  }
+}
