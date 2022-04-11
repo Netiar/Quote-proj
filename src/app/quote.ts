af70931288e0c9928bee395325262b6005196d35
Raindrop <collins.odinga@student.moringaschool.com>
2022-04-12 00:26:41 +0300

    modified app quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,5 @@
 export class Quote {
+   
     constructor(public id:number, public quote:string,public details:string,public upvote:number,public downvote:number,public completeDate: Date ,){
         this.id =id;
         this.quote = quote
